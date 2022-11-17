1a669126e68de21439297f7ef629200252e8c1f6
Phillips M (FCES) <23045401@students.southwales.ac.uk>
2022-11-17 16:43:35 +0000

    Feature: added functions

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,23 @@
+
 a = int(input("What would you like your first number to be?"))
 b = int(input("What would you like your second number to be?"))
 
-c = a + b
+#addition
+def addition(a,b):
+    c=a+b
+    return c
+def subtraction(a,b):
+    c=a-b
+    return c
+def multiplication(a,b):
+    c=a*b
+    return c
+def division_result(a,b):
+    c=a/b
+    return c
+def ratio(a,b,format='Decimal'):
+    if format == 'Decimal':
+        c = f"The ratio of {a} to {b} is {a/b}:1"
+    if format == 'Percentage':
+        c = f"{a} is {a/addition(a,b) * 100}% of {b}"
+    return c
